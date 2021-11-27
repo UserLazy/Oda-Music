005faa51d60b6f46bcaae8504773b1d827cdcfd6
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-27 20:53:30 +0700

    config

---
+++ oda/config.py
@@ -0,0 +1,30 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSID = getenv("ASSID")
+ASSNAME = getenv("ASSNAME")
+ASSUSERNAME = getenv("ASSUSERNAME")
+BOT_ID = getenv("BOT_ID")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
+
+CMD_MUSIC = list(getenv("CMD_MUSIC", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
