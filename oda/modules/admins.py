928799898cd60e6ed7fd6015520303315f147e74
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-27 19:39:29 +0700

    rework

---
+++ oda/modules/admins.py
@@ -0,0 +1,147 @@
+from asyncio import QueueEmpty
+
+from pytgcalls.types.input_stream import InputAudioStream
+from pytgcalls.types.input_stream import InputStream
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from oda import app
+from oda.config import que
+from oda.tgcalls import calls
+from oda.utils.filters import command, other_filters
+from oda.utils.decorators import sudo_users_only, errors
+from oda.tgcalls.queues import clear, get, is_empty, put, task_done
+
+
+async def member_permissions(chat_id: int, user_id: int):
+    perms = []
+    try:
+        member = await app.get_chat_member(chat_id, user_id)
+    except Exception:
+        return []
+    if member.can_post_messages:
+        perms.append("can_post_messages")
+    if member.can_edit_messages:
+        perms.append("can_edit_messages")
+    if member.can_delete_messages:
+        perms.append("can_delete_messages")
+    if member.can_restrict_members:
+        perms.append("can_restrict_members")
+    if member.can_promote_members:
+        perms.append("can_promote_members")
+    if member.can_change_info:
+        perms.append("can_change_info")
+    if member.can_invite_users:
+        perms.append("can_invite_users")
+    if member.can_pin_messages:
+        perms.append("can_pin_messages")
+    if member.can_manage_voice_chats:
+        perms.append("can_manage_voice_chats")
+    return perms
+
+from oda.utils.administrator import adminsOnly
+
+
+@app.on_message(command(["pause", "op"]) & other_filters)
+@adminsOnly
+@sudo_users_only
+async def pause(_, message: Message):
+    checking = message.from_user.mention
+    chat_id = message.chat.id
+    if not await is_active_chat(chat_id):
+        return await message.reply_text(
+            "I dont think if something's playing on voice chat"
+        )
+    elif not await is_music_playing(message.chat.id):
+        return await message.reply_text(
+            "I dont think if something's playing on voice chat"
+        )
+    await music_off(chat_id)
+    await calls.pytgcalls.pause_stream(chat_id)
+    await message.reply_text(f"🎧 Voicechat Paused by {checking}!")
+
+
+@app.on_message(command(["resume", "or"]) & other_filters)
+@adminsOnly
+@sudo_users_only
+async def resume(_, message: Message):
+    checking = message.from_user.mention
+    chat_id = message.chat.id
+    if not await is_active_chat(chat_id):
+        return await message.reply_text(
+            "I dont think if something's playing on voice chat"
+        )
+    elif await is_music_playing(chat_id):
+        return await message.reply_text(
+            "I dont think if something's playing on voice chat"
+        )
+    else:
+        await music_on(chat_id)
+        await calls.pytgcalls.resume_stream(chat_id)
+        await message.reply_text(f"🎧 Voicechat Resumed by {checking}!")
+
+
+@app.on_message(command(["end", "oe"]) & other_filters)
+@adminsOnly
+@sudo_users_only
+async def stop(_, message: Message):
+    checking = message.from_user.mention
+    chat_id = message.chat.id
+    if await is_active_chat(chat_id):
+        try:
+            clear(chat_id)
+        except QueueEmpty:
+            pass
+        await remove_active_chat(chat_id)
+        await calls.pytgcalls.leave_group_call(chat_id)
+        await message.reply_text(f"🎧 Voicechat End/Stopped by {checking}!")
+    else:
+        return await message.reply_text(
+            "I dont think if something's playing on voice chat"
+        )
+
+
+@app.on_message(command(["skip", "os"]) & other_filters)
+@adminsOnly
+@sudo_users_only
+async def skip(_, message: Message):
+    checking = message.from_user.mention
+    chat_id = message.chat.id
+    chat_title = message.chat.title
+    if not await is_active_chat(chat_id):
+        await message.reply_text("Nothing's playing on Music")
+    else:
+        task_done(chat_id)
+        if is_empty(chat_id):
+            await remove_active_chat(chat_id)
+            await message.reply_text("No more music in Queue \n\nLeaving Voice Chat")
+            await calls.pytgcalls.leave_group_call(chat_id)
+            return
+        else:
+            await calls.pytgcalls.change_stream(
+                chat_id,
+                InputStream(
+                    InputAudioStream(
+                        get(chat_id)["file"],
+                    ),
+                ),
+            )
+            await message.reply_text("⏭ **You've skipped to the next song.**")
+
+
+@app.on_message(filters.command(["cleandb", "oc"]))
+@adminsOnly
+@sudo_users_only
+async def stop_cmd(_, message):
+    chat_id = message.chat.id
+    try:
+        clear(chat_id)
+    except QueueEmpty:
+        pass
+    await remove_active_chat(chat_id)
+    try:
+        await calls.pytgcalls.leave_group_call(chat_id)
+    except:
+        pass
+    await message.reply_text("Erased Databae, Queues, Logs, Raw Files, Downloads.")
