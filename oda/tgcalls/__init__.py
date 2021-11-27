b8a4e8ea0bf94efb09345fb70b9de9f3ce4c87f6
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-27 18:58:02 +0700

    rework

---
+++ oda/tgcalls/__init__.py
@@ -0,0 +1,9 @@
+from os import listdir, mkdir
+from pyrogram import Client
+from oda import config
+from oda.tgcalls.queues import (clear, get, is_empty, put, task_done)
+from oda.tgcalls.downloader import download
+from oda.tgcalls.convert import convert
+from oda.tgcalls.calls import run
+from oda.tgcalls.calls import client
+client = 1
