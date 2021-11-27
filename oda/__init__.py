7173562ed13cddd68694303449d0d7f210acf563
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-27 20:49:02 +0700

    kkk

--- oda/__init__.py
+++ oda/__init__.py
@@ -1,1 +1,29 @@
+import asyncio
+import importlib
+from pyrogram import Client
+from aiohttp import ClientSession
+from oda import config
 
+
+SUDO_USERS = config.SUDO_USERS
+OWNER_ID = config.OWNER_ID
+BOT_ID = config.BOT_ID
+BOT_NAME = ""
+BOT_USERNAME = ""
+ASSID = config.ASSID
+ASSNAME = ""
+ASSUSERNAME = ""
+
+app = Client(
+    'odamusic',
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+aiohttpsession = ClientSession()
+
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+
+app.start()
+client.start()
