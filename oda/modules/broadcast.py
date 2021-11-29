f1d4b954f7fb88e0da468a8cceb5ee2097317821
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-29 09:45:19 +0700

    add gcast without pin msg

--- oda/modules/broadcast.py
+++ oda/modules/broadcast.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from pyrogram import filters
+from pyrogram.errors FloodWait
 
 from oda import app
 from oda.config import SUDO_USERS
@@ -76,3 +77,29 @@ async def broadcast_message(_, message):
         except Exception:
             pass
     await message.reply_text(f"**Broadcasted Message In {sent} Chats and {pin} Pins.**")
+    
+    
+# Broadcast without pinned
+
+@app.on_message(command("broadcast") & filters.user(SUDOERS) & ~filters.edited)
+async def broadcast_message(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Usage**:\n/broadcast <message>")
+    sleep_time = 0.1
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    schats = await get_served_chats()
+    chats = [int(chat["chat_id"]) for chat in schats]
+    m = await message.reply_text(
+        f"Broadcast in progress, will take {len(chats) * sleep_time} seconds."
+    )
+    for i in chats:
+        try:
+            await app.send_message(i, text=text)
+            await asyncio.sleep(sleep_time)
+            sent += 1
+        except FloodWait as e:
+            await asyncio.sleep(int(e.x))
+        except Exception:
+            pass
+    await m.edit(f"**Broadcasted Message In {sent} Chats.**")
