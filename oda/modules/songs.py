fe9d8162bcd8fc83ee29b3a91298dda17cac92c5
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-30 12:09:50 +0700

    nothing

--- oda/modules/songs.py
+++ oda/modules/songs.py
@@ -47,7 +47,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("📥 `Downloading Song...`\n\n⏱ `Please wait...`")
+    m.edit("`Downloading Song, Please wait...⏱`")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
