82cab9893993c885e71a3500d67e9bc2936e6328
UserLazy <78595134+UserLazy@users.noreply.github.com>
2025-10-02 21:27:18 +0700

    Delete set_git.sh

--- set_git.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-set -euo pipefail
-NAME="${1:-UserLazy}"
-EMAIL="${2:-you@example.com}"
-git config --global user.name  "$NAME"
-git config --global user.email "$EMAIL"
-git config --global credential.helper "cache --timeout=7200"
-echo "✅ Git global configured."
